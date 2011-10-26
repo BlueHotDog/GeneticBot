d1ec96f16fb7ac3c715de500ce267b0c769b01dd
BlueHotDog <dannifriedland@gmail.com>
2011-10-26 21:50:30 +0200

    execute working

--- gp/tree.js
+++ gp/tree.js
@@ -16,7 +16,15 @@ _.extend(Tree.prototype, {
 		console.log("%j",this);
 	},
 	execute: function() {
-		
+		var params = [];
+		this.nodes.forEach(function(node) {
+			params.push(node.execute());
+		});
+		if(_(this.value).isFunction()===false){
+			return this.value;
+		} else {
+			return this.value.apply(this,params);
+		}
 	}
 });
 
@@ -29,4 +37,5 @@ tree.addNode(10);
 tree.addNode(5);
 
 tree.print();
+console.log(tree.execute());
 
